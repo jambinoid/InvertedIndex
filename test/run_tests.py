80e49138cc750b0da78734a90c7e7b942f379e4e
Alexandr Spiridonov <alexandr.spy@gmail.com>
2021-12-08 15:44:26 +0300

    Changed testing library, removed api from testing, added new tests.

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,19 +1,15 @@
 import subprocess
 import psycopg2
 
-#from ..inverted_index import InvertedIndex, PostgresConfig
-import requests
-import logging
-logging.getLogger("requests").setLevel(logging.CRITICAL)
+from inverted_index import InvertedIndex, PostgresConfig
 
-import os
-import signal
 from os import listdir
-from os.path import isfile, join, basename
+from os.path import isfile, join
 from time import sleep
 
-import unittest
-import ast
+import pytest
+import time
+
 
 from dotenv import dotenv_values
 
@@ -21,6 +17,11 @@ CONFIG = {
     **dotenv_values('.env'),
     **dotenv_values('.env.local'),
 }
+
+_ENCODINGS = [None,'gamma','delta']
+_INDEX = None
+
+#----------------- CONTAINER METHODS-----------------
 
 def initialize_psql_container():
     '''
@@ -47,7 +48,7 @@ def initialize_psql_container():
     cur = conn.cursor()
     
     try:
-        cur.execute("CREATE TABLE pages (url varchar NOT NULL UNIQUE, parsed_data varchar NOT NULL);")
+        cur.execute("CREATE TABLE pages (id serial PRIMARY KEY,url varchar NOT NULL UNIQUE, parsed_data varchar NOT NULL);")
     except:
         print("I can't drop our test database!")
 
@@ -56,90 +57,196 @@ def initialize_psql_container():
     test_file_dir = 'test_files' # to run from root
     test_files = [join(test_file_dir, f) for f in listdir(test_file_dir) if isfile(join(test_file_dir, f))]
 
+
     for filename in test_files:
         with open(filename,'r',encoding = 'utf-8') as f:
-            cur.execute("INSERT INTO pages VALUES(%s,%s);",(filename,f.read()))
+            cur.execute("INSERT INTO pages (url,parsed_data) VALUES(%s,%s);",(filename,f.read()))
 
 
     conn.commit() # <--- makes sure the change is shown in the database
     conn.close()
     cur.close()
 
-def start_api():
-    #Starting api
-    subprocess.run('python3 ../tools/create_index.py --config test_config.json'.split())
-    api_pid = subprocess.Popen('python3 ../api.py --config test_config.json'.split(), preexec_fn=os.setsid)
-    sleep(5)
-    return(os.getpgid(api_pid.pid))
-    
-def stop_api(api_pid):
-    os.killpg(api_pid, signal.SIGTERM)
-
 def stop_psql_container():
     '''
     Stops and deletes docker compose container. 
     '''
+     #print('stopping container')
     subprocess.run(['sudo','docker-compose','rm','-fsv'])
 
-def send_api_request(word):
-    r = requests.get(f'http://localhost:5005/search?q={word}&top=10')
-    #print(f'Query: {word}\nStatus: {r.status_code}\nElapsed: {r.elapsed.total_seconds()}\nResult: {r.text}')
-    return r
+#----------------- SEARCH METHODS-----------------
 
+def create_inv_idx(encoding = None):
+    '''
+    Creates instanse of Inverted Index. Won't work without calculating index.
+    '''
+    search_config = PostgresConfig(
+        database = CONFIG['POSTGRES_DB'], 
+        user = CONFIG['POSTGRES_USER'], 
+        password = CONFIG['POSTGRES_PASSWORD'], 
+        host = "localhost", 
+        port = "5600")
 
+    pytest._IINDEX = InvertedIndex(
+        user=search_config.user,
+        password=search_config.password,
+        host=search_config.host,
+        port=search_config.port,
+        database=search_config.database,
+        iindex_table="inverted_index_gamma",
+        iindex_term_col=search_config.iindex_term_col,
+        iindex_docid_col=search_config.iindex_docid_col,
+        iindex_count_col=search_config.iindex_count_col,
+        dlens_table="page_lengths_gamma",
+        dlens_docid_col=search_config.dlens_docid_col,
+        dlens_len_col=search_config.dlens_len_col,
+        src_table=search_config.src_table,
+        src_docid_col=search_config.src_docid_col,
+        src_doc_col=search_config.src_doc_col,
+        encoding=encoding,
+        clean=True
+    )
 
+def calculate_index():
+    pytest._IINDEX.create()
 
-class RankingTests(unittest.TestCase):
-    api_pid = 0
-    @classmethod
-    def setUpClass(cls):
-        initialize_psql_container()
-        cls.api_pid = start_api()
-        print(f'Api is running under PID - {cls.api_pid}')
+def clean_indexes():
+    pytest._IINDEX.remove(with_additional_tables=True)
+
+def prepare_search(encoding = None):
+    '''
+    Prepares database and test system for search.
+    If index already exists recreates it if encoding is different.
+    '''
+    if pytest._IINDEX is not None:
+        if pytest._IINDEX.encoding != encoding:
+            clean_indexes()
+            del pytest._IINDEX
+        else:
+            return
+
+    create_inv_idx(encoding)
+    calculate_index()
+
+def search(term):
+    return pytest._IINDEX.search(
+        query=term,
+        col_to_return='url'
+    )
+
+#----------------- TEST PREPARATIONS -----------------
+
+@pytest.fixture(scope="session", autouse=True)
+def preparation(request):
+    # start psql container using docker-compose
+    initialize_psql_container()
+    # create global variable to remember search reference and create inverted index
+    pytest._IINDEX = None
+    prepare_search()
+    # after all tests, close anb delete container
+    request.addfinalizer(stop_psql_container)
+
+#----------------- TESTS -----------------
+
+def test_finding_right():
         
-    @classmethod
-    def tearDownClass(cls):
-        stop_api(cls.api_pid)
-        stop_psql_container()
+    test_word = 'шаман'
+    document = "test_files/shaman_king.txt"
 
-    def test_word_search(self):
-        test_word = 'шаман'
-        document = "test_files/shaman_king.txt"
-        wrong_doc = "test_files/borsch.txt"
+    req = search(test_word)
+    #Testing finding right document
+    assert document in req
 
-        req = send_api_request(test_word)
-        #Testing finding right document
-        self.assertIn(document,req.text)
-        # Testing not finding wrong document
-        self.assertNotIn(wrong_doc,req.text)
-
-    def test_order(self):
-        test_word = 'земля'
-        ordered_docs = ["test_files/additional_text.txt","test_files/walking_to_river.txt","test_files/heart_stopped.txt"]
-
-        req = send_api_request(test_word)
-
-        d2 = ast.literal_eval(req.text)
-
-        self.assertEqual(d2['data'],ordered_docs)
-
-    def test_search_time(self):
-        test_words = ['шаман','борщ','свет',"земля","планета","гармония"]
+def test_finding_wrong():
         
-        # List of request times
-        search_times =[]
-        for word in test_words:
-            search_times.append(send_api_request(word).elapsed.total_seconds())
+    test_word = 'шаман'
+    wrong_doc = "test_files/borsch.txt"
 
-        #Testing finding right document
-        mean_time = sum(search_times) / len(search_times)
-        print(f'Mean search time: {mean_time}')
-        self.assertLess(mean_time,3)
+    req = search(test_word)
+    #Testing finding right document
+    assert wrong_doc not in req
 
+def test_order():
 
+    test_word = 'земля'
+    ordered_docs = ["test_files/earth.txt","test_files/walking_to_river.txt","test_files/mars.txt","test_files/heart_stopped.txt"]
 
-
-if __name__ == "__main__":
-
-    unittest.main()
+    search_res = search(test_word)
     
+    print(search_res)
+    assert search_res == ordered_docs
+
+def test_encodings_same_ranking():
+    rankings= []
+    test_word = 'земля'
+
+    # getting default encoding ranks and killing api
+    for code in _ENCODINGS:
+        prepare_search(code)
+        rankings.append(search(test_word))
+    
+    assert all(ranks == rankings[0] for ranks in rankings)
+
+def test_search_time_default():
+    prepare_search()
+
+    test_words = ['шаман','борщ','свет',"земля","планета","гармония"]
+    times = []
+    for word in test_words:
+        t1 = time.time()
+        search(word)
+        t2 = time.time()
+        times.append(t2-t1)
+
+    mean_time = sum(times)/len(times)
+    #print(mean_time)
+    
+    assert mean_time< 3
+
+def test_search_time_default():
+    prepare_search()
+
+    test_words = ['шаман','борщ','свет',"земля","планета","гармония"]
+    times = []
+    for word in test_words:
+        t1 = time.time()
+        search(word)
+        t2 = time.time()
+        times.append(t2-t1)
+
+    mean_time = sum(times)/len(times)
+    #print(mean_time)
+    
+    assert mean_time< 3
+
+def test_search_time_gamma():
+    prepare_search(_ENCODINGS[1])
+
+    test_words = ['шаман','борщ','свет',"земля","планета","гармония"]
+    times = []
+    for word in test_words:
+        t1 = time.time()
+        search(word)
+        t2 = time.time()
+        times.append(t2-t1)
+
+    mean_time = sum(times)/len(times)
+    #print(mean_time)
+    
+    assert mean_time< 3
+
+def test_search_time_delta():
+    prepare_search(_ENCODINGS[2])
+
+    test_words = ['шаман','борщ','свет',"земля","планета","гармония"]
+    times = []
+    for word in test_words:
+        t1 = time.time()
+        search(word)
+        t2 = time.time()
+        times.append(t2-t1)
+
+    mean_time = sum(times)/len(times)
+    #print(mean_time)
+    
+    assert mean_time< 3
